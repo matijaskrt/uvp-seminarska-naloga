d934f37062b43dd60b93c36d2e510d16d944acc4
Matija Skrt <matija.skrt@gmail.com>
2024-07-24 12:28:45 +0200

    regex nadaljevanje

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -1,29 +1,50 @@
 import re
 import requests
 
-
 VSEBINA = ""
-
-for x in ["pre1500", "1500", "1600", "1700", "1800-1824", "1825-1849"]:
-    r = requests.get(f"https://music.library.appstate.edu/lute/{x}")
-    VSEBINA += r.text
-#ustvarimo seznam
-SEZNAM = VSEBINA.split("</li></ul></li><li>")
+SEZNAM = []
+for x in ["pre1500", "1500s", "1600s", "1700s", "1800s", "1800-1824", "1825-1849", "nd"]:
+    r = requests.get(f"https://music.library.appstate.edu/lute/1500s")
+    VSEBINA = r.text[r.text.index("<li>"):]
+    SEZNAM += VSEBINA.split("</li></ul></li><li>")
 
 LETO_RE = r"(\d+\-*\d*)[a-z\[]*<br>"
 AVTOR_RE = r"<br>(.+)\."
-MESTO_RE = r""
-DRŽAVA_RE = r"</em>[^\(]*\(([^<:]*)[:<]+"
-INSTRUMENT_RE = r""
+KRAJ_RE = r"</em>[^\(]*\(([^<:]*)[:<]+"
 
-print(SEZNAM[59])
+print(len(SEZNAM))
 
+with open("zbrani_podatki.txt", "w", encoding="UTF-8") as izhod:
+    for DELO in SEZNAM:
+        RAZDELJEN = DELO.split("<ul><li>")
+        if re.search(LETO_RE, RAZDELJEN[0]) == None:
+            LETO = "NONE"
+        else:
+            LETO = re.search(LETO_RE, RAZDELJEN[0]).group(1)
+        if re.search(AVTOR_RE, RAZDELJEN[0]) == None:
+            AVTOR = "NONE"
+        else:
+            AVTOR = re.search(AVTOR_RE, RAZDELJEN[0]).group(1)
+        if re.search(KRAJ_RE, RAZDELJEN[0]) == None:
+            KRAJ = "NONE"
+        else:
+            KRAJ = re.search(KRAJ_RE, RAZDELJEN[0]).group(1)
 
+        if ", " in KRAJ:
+            MESTO = KRAJ.split(", ")[0]
+            DRŽAVA = KRAJ.split(", ")[1][:-1]
+        else:
+            MESTO = "NONE"
+            DRŽAVA = KRAJ
+        #TABLATURE = re.search(TABLATURE_RE, RAZDELJEN[1]).group(1)
+        izhod.write(f"\n{LETO}, {AVTOR}, {MESTO}, {DRŽAVA}")
+#lahko bi vsak člen razdelili na dva dela: nad alinejami in pod njimi z <ul><li>
 
+#TABLATURE_RE = r"\b(\B*\btablature)"
+#INSTRUMENT_RE = r""
+
+#print(SEZNAM[25])
 #vpis v datoteko
-#with open("urejeni_podatki", "w", encoding=utf-8) as izhod:
-#   for delo in seznam:
-#       izhod.write()
 
 #zacetek = </li></ul></li><li>
 #leto = na koncu pred <br>
