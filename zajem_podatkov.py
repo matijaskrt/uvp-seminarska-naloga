fcc6b46d0ee050dd99d781ae626ec836695d2f27
Matija Skrt <matija.skrt@gmail.com>
2024-08-23 12:59:26 +0200

    lintanje in formatiranje dokončano

--- zajem_podatkov.py
+++ zajem_podatkov.py
@@ -1,116 +1,129 @@
+"""Knjižnice in datoteke, ki jih potrebujemo."""
 import re
-import requests
 import csv
+import requests
+import dodatki as dd
 
-#poberemo podatke
+
+# Poberemo podatke.
 VSEBINA = ""
 SEZNAM = []
-for x in ["pre1500", "1500s", "1600s", "1700s", "1800s", "1800-1824", "1825-1849", "nd"]:
-    r = requests.get(f"https://music.library.appstate.edu/lute/{x}")
+for x in dd.LETNICE:
+    try:
+        r = requests.get(f"https://music.library.appstate.edu/lute/{x}",
+                         timeout=10)
+    except requests.exceptions.Timeout:
+        print("Čas je potekel.")
     VSEBINA = r.text[r.text.index("<li>"):]
     SEZNAM += VSEBINA.split("</li></ul></li><li>")
 
-POSEBNI_ZNAKI = {"&#382;": "ž", "&ocirc;": "ô", "&#347;": "ś", "&szlig;": "ß", "&#324": "ń", "&#328;": "n", "&#322;": "ł", "&#345;": "ř", "&#269;": "č", "&yacute;": "ý", "&ecirc;": "ê", "&icirc;": "î", "&aring;": "å", "&atilde;": "ã", "&ntilde;": "ñ", "&nbsp;": "", "&euml;": "ë", "&iuml;": "ï", "&ouml;": "ö", "&auml;": "ä", "&uuml;": "ü", "e&#769;": "é", "&eacute;": "é", "&oacute;": "ó", "&aacute;": "á", "&iacute;": "í", "&ograve;": "ò", "&egrave;": "è", "&agrave;": "à", "&ccedil;": "ç", "&Scaron;": "Š", "&scaron;": "š"}
-
-print(SEZNAM[1713])
-
-#Regularni izrazi za določen tip podatkov.
-LETO_RE = r"([^\-]\d{4,}\??\/?\-?[a-z]*\s?\d*)[\;\:\(\)\sA-Za-z\]\[\&]*\s?[A-Za-z\]\[<\/>\?\]]*<br>"
-AVTOR_RE = r"<br>([\d\#A-Za-z\'\s\?\;\&\-]{3,}\,?\s[\d\#A-Za-z\'\s\?\;\&\-]{3,})\.*"
-KRAJ_RE1 = r"</em>[^\(]*\(([A-Z]+[A-Za-z\s\,\[\;\&]*)[\)\:\]]+"
-KRAJ_RE2 = r"<br>[^\(\[<>]*\(([A-Z]+[A-Za-z\s\,\[\;\&]*)[\)\:\]]+"
-TABLATURA_RE = r"\w{3,}\stablature" 
-LUTNJA_RE = r"\d+-course\s[A-Za-z]*\b"
-TREATISE_RE = r"treatise"
-
+# Odpremo datoteko zbrani_podatki.csv, v katero bomo pisali podatke.
 with open("zbrani_podatki.csv", "w", newline='', encoding="utf-8") as izhod:
     writer = csv.writer(izhod)
-    writer.writerow(["ID", "LETO", "AVTOR", "MESTO", "DRŽAVA", "TABLATURA", "INSTRUMENT", "STRUNE" , "RAZPRAVA"])
+    # Zapišemo prvo vrsto, z opisom podatkov v stolpcu.
+    writer.writerow(["ID", "LETO", "AVTOR", "MESTO", "DRŽAVA",
+                     "TABLATURA", "INSTRUMENT", "STRUNE" , "RAZPRAVA"])
     ID = 0
-    for DELO in SEZNAM:
-        RAZDELJEN = DELO.split("<ul><li>")
+
+    # Sedaj iz vsakega dela izluščimo želene podatke.
+    for delo in SEZNAM:
+
+        # Besedilo, ki opisuje eno izmed del na spletni strani,
+        # razdelimo na dva dela. Podatki bodo tako natančneje
+        # pridobljeni, saj se večina vedno pojavlja v istem delu.
+        DELITEV = delo.split("<ul><li>")
+        PRVI_DEL = DELITEV[-2]
+        DRUGI_DEL = DELITEV[-1]
+
+        # Leto izdaje.
         LETO = None
-        if "<br>" in RAZDELJEN[-2]:
-            if re.search(LETO_RE, RAZDELJEN[-2][:RAZDELJEN[-2].index("<br>") + 4]) == None:
-                LETO = 0
-            else:
-                if "-" in re.search(LETO_RE, RAZDELJEN[-2][:RAZDELJEN[-2].index("<br>") + 4]).group(1) or "/" in re.search(LETO_RE, RAZDELJEN[-2]).group(1):
-                    LETO = 0
-                elif "?" in re.search(LETO_RE, RAZDELJEN[-2][:RAZDELJEN[-2].index("<br>") + 4]).group(1):
-                    LETO = 0
-                else:
-                    LETO = int(re.search(LETO_RE, RAZDELJEN[-2][:RAZDELJEN[-2].index("<br>") + 4]).group(1)[1:5])
-        if re.search(AVTOR_RE, RAZDELJEN[-2]) == None:
+        if "<br>" in PRVI_DEL:
+            LETO_ISKANJE = PRVI_DEL[:PRVI_DEL.index("<br>") + 4]
+            if re.search(dd.LETO_RE, LETO_ISKANJE) is not None:
+                if ("-" not in re.search(dd.LETO_RE, LETO_ISKANJE).group(1)
+                    and "/" not in re.search(dd.LETO_RE, PRVI_DEL).group(1)
+                    and "?" not in re.search(dd.LETO_RE, LETO_ISKANJE).group(1)):
+                    LETO = int(re.search(dd.LETO_RE, LETO_ISKANJE).group(1)[1:5])
+
+        # Avtor dela.
+        if re.search(dd.AVTOR_RE, PRVI_DEL) is None:
             AVTOR = None
         else:
-            AVTOR = re.search(AVTOR_RE, RAZDELJEN[-2]).group(1).strip()
+            AVTOR = re.search(dd.AVTOR_RE, PRVI_DEL).group(1).strip()
 
-        if re.search(KRAJ_RE1, RAZDELJEN[-2]) == None:
-                KRAJ = None
-        else:
-            KRAJ = re.search(KRAJ_RE1, RAZDELJEN[-2]).group(1)
-            if ", " in KRAJ:
-                MESTO = re.sub(r"[^A-Za-z\?\s\&\;\:]+", "", KRAJ.split(", ")[0])
-                DRŽAVA = re.sub(r"[^A-Za-z\?\s\&\;\:]+", "", KRAJ.split(", ")[1])
-            else:
-                MESTO = None
-                DRŽAVA = re.sub(r"[^A-Za-z\?\s\&\;\:]+", "", KRAJ)
-        if KRAJ == None:      
-            if re.search(KRAJ_RE2, RAZDELJEN[-2]) == None:
-                KRAJ, MESTO, DRŽAVA = None, None, None
-            else:
-                KRAJ = re.search(KRAJ_RE2, RAZDELJEN[-2]).group(1)
-                if ", " in KRAJ:
-                    MESTO = re.sub(r"[^A-Za-z\?\s\&\;\:]+", "", KRAJ.split(", ")[0])
-                    DRŽAVA = re.sub(r"[^A-Za-z\?\s\&\;\:]+", "", KRAJ.split(", ")[1])
-                else:
-                    MESTO = None
-                    DRŽAVA = re.sub(r"[^A-Za-z\?\s\&\;\:]+", "", KRAJ)
+        # Mesto in država izdaje.
+        KRAJ_SUB = r"[^A-Za-z\?\s\&\;\:]+"
+        KRAJ, MESTO, DRZAVA = None, None, None
+        # Če v prvem ciklu ne najdemo kraja, poskusimo še z drugim
+        # regexom. Kraji se pojavljajo v različnih delih datoteke,
+        # zato jih poskusimo natančneje zajeti.
+        for kraj_re in [dd.KRAJ_RE1, dd.KRAJ_RE2]:
+            if KRAJ is None:
+                if re.search(kraj_re, PRVI_DEL) is not None:
+                    KRAJ = re.search(kraj_re, PRVI_DEL).group(1)
+                    if ", " in KRAJ:
+                        MESTO = re.sub(KRAJ_SUB, "", KRAJ.split(", ")[0])
+                        DRZAVA = re.sub(KRAJ_SUB, "", KRAJ.split(", ")[1])
+                    else:
+                        MESTO = None
+                        DRZAVA = re.sub(KRAJ_SUB, "", KRAJ)
 
-        TABLATURA_SET = set(re.findall(TABLATURA_RE, RAZDELJEN[-1]))
+        # Tablatura v kateri je delo napisano.
+        TABLATURA_SET = set(re.findall(dd.TABLATURA_RE, DRUGI_DEL))
         TABLATURA = ""
         for tab in TABLATURA_SET:
-            TABLATURA += f"{tab}, "
+            TABLATURA = "".join([TABLATURA, f"{tab}, "])
         if TABLATURA == "":
             TABLATURA = None
-        if TABLATURA != None:
+        if TABLATURA is not None:
             TABLATURA = TABLATURA.strip(", ")
 
-        STRUNE_SET = set(re.findall(r"[\d\,and\-\s]+string", RAZDELJEN[-1])) | set(re.findall(r"[\d\,and\-\s]+course", RAZDELJEN[-1]))
-            
+        # Vrsta instrumenta, za katerega je delo napisano.
+        LUTNJA_SET = (set(re.findall(r"lute", DRUGI_DEL)) |
+                      set(re.findall(r"guitar", DRUGI_DEL)))
+        LUTNJA = ""
+        for lute in LUTNJA_SET:
+            LUTNJA = ", ".join([LUTNJA, f"{lute}"])
+        if LUTNJA == "":
+            LUTNJA = "drugo"
+        INSTRUMENT = LUTNJA.strip(", ")
+
+        # Število (parov) strun, ki jih ima instrument,
+        # za katerega je delo napisano.
+        STRUNE = (set(re.findall(dd.STRING_RE, DRUGI_DEL)) |
+                  set(re.findall(dd.COURSE_RE, DRUGI_DEL)))
         STEVILO_STRUN_SET = set()
-        if STRUNE_SET != set():
-            for element in STRUNE_SET:
+        if STRUNE is not set():
+            for element in STRUNE:
                 stevilo = re.sub(r"[^\d\s]+", "", element)
                 STEVILO_STRUN_SET |= set(stevilo.split(" "))
-            
         STEVILO_STRUN = ""
-        if STEVILO_STRUN_SET != set():
-            STEVILO_STRUN_SET = sorted([x for x in STEVILO_STRUN_SET])
+        if STEVILO_STRUN_SET is not set():
+            STEVILO_STRUN_SET = list(STEVILO_STRUN_SET)
             for element in STEVILO_STRUN_SET:
                 STEVILO_STRUN += f"{element}, "
             STEVILO_STRUN = STEVILO_STRUN.strip(", ")
         if STEVILO_STRUN == "":
             STEVILO_STRUN = None
 
-        LUTNJA_SET = set(re.findall(r"lute", RAZDELJEN[-1])) | set(re.findall(r"guitar", RAZDELJEN[-1]))
-        LUTNJA = ""
-        for lute in LUTNJA_SET:
-            LUTNJA += f"{lute}, "
-        if LUTNJA == "":
-            LUTNJA = "drugo"
-        INSTRUMENT = LUTNJA.strip(", ")
-
-        if len(re.findall(TREATISE_RE, RAZDELJEN[-1],)) == 0:
+        # Preverimo, če delo vsebuje razpravo ali ne.
+        if len(re.findall(dd.TREATISE_RE, DRUGI_DEL,)) == 0:
             RAZPRAVA = "NE"
         else:
             RAZPRAVA = "DA"
-        VRSTA = [ID, LETO, AVTOR, MESTO, DRŽAVA, TABLATURA, INSTRUMENT, STEVILO_STRUN, RAZPRAVA]
+
+        # Vrsta, kot bo izpisana v csv dokumentu.
+        VRSTA = [ID, LETO, AVTOR, MESTO, DRZAVA, TABLATURA,
+                 INSTRUMENT, STEVILO_STRUN, RAZPRAVA]
+
+        # Preverimo še posebne znake in jih zamenjamo.
         for indeks in range(2, len(VRSTA)):
             kos = VRSTA[indeks]
-            for znak in POSEBNI_ZNAKI:
-                if kos != None:
-                    kos = re.sub(znak, POSEBNI_ZNAKI[znak], kos)
+            for znak, koda in dd.POSEBNI_ZNAKI.items():
+                if kos is not None:
+                    kos = re.sub(znak, koda, kos)
             VRSTA[indeks] = kos
+
+        # Zapišemo izluščene podatke v datoteko zbrani_podatki.csv.
         writer.writerow(VRSTA)
         ID +=1
